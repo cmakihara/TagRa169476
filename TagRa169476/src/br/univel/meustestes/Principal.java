3f6ce1353dca3ed7b39aaf17f8fc8dbc7081c096
clmakihara <clmakihara@gmail.com>
2017-04-29 15:09:42 -0300

    todos os filhos

--- TagRa169476/src/br/univel/meustestes/Principal.java
+++ TagRa169476/src/br/univel/meustestes/Principal.java
@@ -11,18 +11,18 @@ public class Principal {
 
 	public Principal() {
 		
-		Conta contaAgua = new Conta(1, "Água", new BigDecimal("101,28"));
+		//despesas administrativas Pai 01
+		Conta contaAgua = new Conta(1, "Água", new BigDecimal("50,00"));
 		UniNode<Conta> noAgua = new UniNodeImpl<>(contaAgua);
 		
-		Conta contaAluguel = new Conta(2, "Aluguel", new BigDecimal("900,00"));
+		Conta contaAluguel = new Conta(2, "Aluguel", new BigDecimal("615,00"));
 		UniNode<Conta> noAlu = new UniNodeImpl<>(contaAluguel);
 		
-		Conta contaIntTel = new Conta(3, "Internet e Telefone", new BigDecimal("165,35"));
+		Conta contaIntTel = new Conta(3, "Internet e Telefone", new BigDecimal("80,00"));
 		UniNode<Conta> noIntTel = new UniNodeImpl<>(contaIntTel);
 		
-		Conta contaEnElet = new Conta(4, "Energia Elétrica", new BigDecimal("252,58"));
-		UniNode<Conta> noEnElet = new UniNodeImpl<>(contaEnElet);
-		
+		Conta contaEnElet = new Conta(4, "Energia Elétrica", new BigDecimal("60,50"));
+		UniNode<Conta> noEnElet = new UniNodeImpl<>(contaEnElet);		
 		
 		Conta despesasAdm = new Conta(1, "Despesas Administrativas", new BigDecimal(0));
 		UniNode<Conta> noDeAdm = new UniNodeImpl<>(despesasAdm);
@@ -30,16 +30,74 @@ public class Principal {
 		noDeAdm.addFilho(noAlu);
 		noDeAdm.addFilho(noIntTel);
 		noDeAdm.addFilho(noEnElet);
+		// Fim despesas administrativas Pai 01
 		
 		
+		// Gastos pessoais Pai 02
+		Conta beneficios = new Conta(1, "Benefícios", new BigDecimal("150,00"));
+		UniNode<Conta> noBenef = new UniNodeImpl<>(beneficios);
+		
+		Conta encargos = new Conta(2, "Encargos", new BigDecimal("130,00"));
+		UniNode<Conta> noEncarg = new UniNodeImpl<>(encargos);
+		
+		Conta salario = new Conta(3, "Salário", new BigDecimal("1500,00"));
+		UniNode<Conta> noSalar = new UniNodeImpl<>(salario);
+		
+		Conta gastosPessoal = new Conta(2, "Gastos com Pessoal", new BigDecimal(0));
+		UniNode<Conta> noGastoPes = new UniNodeImpl<>(gastosPessoal);
+		noGastoPes.addFilho(noBenef);
+		noGastoPes.addFilho(noEncarg);
+		noGastoPes.addFilho(noSalar);
+		// FIM Gastos pessoais Pai 02
+		
+		
+		// Manutencao e Limpeza Pai 03
+		Conta servicoLimpeza = new Conta(1, "Serviços de Limpeza", new BigDecimal("100,00"));
+		UniNode<Conta> noServLimp = new UniNodeImpl<>(servicoLimpeza);
+		
+		Conta servicoManutencao = new Conta(2, "Serviços de Manutenção", new BigDecimal("100,00"));
+		UniNode<Conta> noServManut = new UniNodeImpl<>(servicoManutencao);
+				
+		Conta manutencaoLimpeza = new Conta(3, "Manutenção e Limpeza", new BigDecimal(0));
+		UniNode<Conta> noManutLimpe = new UniNodeImpl<>(manutencaoLimpeza);
+		noManutLimpe.addFilho(noServLimp);
+		noManutLimpe.addFilho(noServManut);
+		// FIM manutencao e limpeza Pai03
+		
+		
+		
+		//Materiais Pai 04
+		Conta materialEscritorio = new Conta(1, "Materiais de Escritório", new BigDecimal("330,00"));
+		UniNode<Conta> noMaterialEscr = new UniNodeImpl<>(materialEscritorio);
+		
+		Conta materialLimpeza = new Conta(2, "Materiais de Limpeza", new BigDecimal("100,00"));
+		UniNode<Conta> noMaterialLimpez = new UniNodeImpl<>(manutencaoLimpeza);
+		
+		Conta materiais = new Conta(4, "Materiais", new BigDecimal(0));
+		UniNode<Conta> noMateriais = new UniNodeImpl<>(materiais);
+		
+		noMateriais.addFilho(noMateriais);
+		noMateriais.addFilho(noMaterialLimpez);
+		//FIM Materiais Pai 04
+		
+		
+		// AVO 01
 		Conta despesasOper = new Conta(1, "Despesas Operacionais", new BigDecimal(0));
 		UniNode<Conta> noDeOpr = new UniNodeImpl<>(despesasOper);
 		noDeOpr.addFilho(noDeAdm);
+		noDeOpr.addFilho(noGastoPes);
+		noDeOpr.addFilho(noManutLimpe);
+		noDeOpr.addFilho(noMateriais);
+		// Avo
+		
+		
+		
+		
 		
 		UniArvore<Conta> planoContas = new UniArvoreImpl(noDeOpr);
 		
 		/**
-		 * ADICIONE AQUI O RESTANTE DAS CONTAS.
+		 * ADICIONE AQUI O RESTANTE DAS CONTAS.					OK acho
 		 */
 		
 		somarFilhos(planoContas);
